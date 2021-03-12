44c28e9156b7e4f84f99cf1a3af315ce77b62610
Isichei Phelim <isicheiphelim@gmail.com>
2021-03-12 15:20:08 +0000

    First commit|

---
+++ src/main/java/com/kepf/Exchangeconectivity/ExchangeConectivityApplication.java
@@ -0,0 +1,13 @@
+package com.kepf.Exchangeconectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeConectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeConectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kepf/Exchangeconectivity/ExchangeConectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kepf.Exchangeconectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeConectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
