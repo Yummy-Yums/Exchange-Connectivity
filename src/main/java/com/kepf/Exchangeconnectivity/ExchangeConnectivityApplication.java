d0ade795ae3fd8e7316c864bd0a4361b359f6f83
Isichei Phelim <isicheiphelim@gmail.com>
2021-03-16 16:18:33 +0000

    Added @RESTController

--- src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
+++ src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
+@RestController
 @RequestMapping(path = "api/v1")
 public class ExchangeConnectivityApplication {
 
