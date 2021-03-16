e1cc14d4e4b32e83f5a571b0186405a67c9cbcc2
Isichei Phelim <isicheiphelim@gmail.com>
2021-03-16 16:32:15 +0000

    Added RequestMap

--- src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
+++ src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
@@ -4,20 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(path = "api/v1")
+
 public class ExchangeConnectivityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeConnectivityApplication.class, args);
 	}
 
-	@GetMapping
+	@RequestMapping(method = {RequestMethod.GET},path = "api/v1")	
 	public String getApi(){
 		return "This is the Exchange-Connectivity Api v1";
 	}
