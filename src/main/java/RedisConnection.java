4de4cefe1ae7365dca16e5e507fa365b2cc06b18
Isichei Phelim <isicheiphelim@gmail.com>
2021-03-16 16:13:41 +0000

    Add mapping

---
+++ src/main/java/RedisConnection.java
@@ -0,0 +1,11 @@
+import redis.clients.jedis.Jedis;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class RedisConnection {
+
+
+
+
+}
--- src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
+++ src/main/java/com/kepf/Exchangeconnectivity/ExchangeConnectivityApplication.java
@@ -2,12 +2,23 @@ package com.kepf.Exchangeconnectivity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RequestMapping(path = "api/v1")
 public class ExchangeConnectivityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeConnectivityApplication.class, args);
+	}
+
+	@GetMapping
+	public String getApi(){
+		return "This is the Exchange-Connectivity Api v1";
 	}
 
 }
